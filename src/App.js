d47fa58cfc4854ed835f36cf6f38d5024ab386da
Devang Banta <you@example.com>
2022-08-19 00:07:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Landing from "./components/Landing";
+import {useState} from 'react'
+import { useEffect } from "react";
+
+function App() {
+
+  
+
+  return (
+  <>
+  <Landing />
+  </>
+  );
+}
+
+export default App;
